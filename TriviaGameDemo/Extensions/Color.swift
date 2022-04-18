26f45bc124a3c62f0054bfcb893e24d320f618e7
Artem Paliutin <pal.artem@icloud.com>
2022-04-18 16:07:59 +0300

    start

---
+++ TriviaGameDemo/Extensions/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  TriviaGameDemo
+//
+//  Created by Artem Paliutin on 18/04/2022.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct ColorTheme {
+    
+}
+
+extension Color {
+    static var colorTheme = ColorTheme()
+}
